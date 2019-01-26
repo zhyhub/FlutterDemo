b99c9319a56452a4b4dbeb988f37ff54f8991c55
sushi1015 <sushi1015@icloud.com>
2019-01-26 23:59:20 +0800

    Draggable 拖拽

---
+++ demo13/lib/draggable_demo.dart
@@ -0,0 +1,45 @@
+import 'package:demo13/draggable_widget.dart';
+import 'package:flutter/material.dart';
+
+class DraggableDemo extends StatefulWidget {
+  _DraggableDemoState createState() => _DraggableDemoState();
+}
+
+class _DraggableDemoState extends State<DraggableDemo> {
+  Color _draggableColor = Colors.grey;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Draggable"),
+      ),
+      body: Stack(
+        children: <Widget>[
+          DraggableWidget(
+            offset: Offset(80.0, 80.0),
+            widgetColor: Colors.tealAccent,
+          ),
+          DraggableWidget(
+            offset: Offset(180.0, 80.0),
+            widgetColor: Colors.purpleAccent,
+          ),
+          Center(
+            child: DragTarget(
+              onAccept: (Color color) {
+                _draggableColor = color;
+              },
+              builder: (context, candidateData, rejectedData) {
+                return Container(
+                  width: 200,
+                  height: 200,
+                  color: _draggableColor,
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
