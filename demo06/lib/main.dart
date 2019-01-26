20d29858603f5882a3677c561923eee6b64e5e26
sushi1015 <sushi1015@icloud.com>
2019-01-26 16:40:56 +0800

    搜索栏

--- demo06/lib/main.dart
+++ demo06/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:demo06/search_bar_demo.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -11,6 +12,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: SearchBarDemo(),
     );
   }
 }
